af0d190c4a229391357e0ff412f0fa0b9e7951c6
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-15 17:03:57 +0000

    hola mundinho

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,50 +1,130 @@
-import React,{useState, useEffect} from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 
 const Home = () => {
 
-	const [todos, setTodos] = useState('')
-	const tareas = [{ label: "Make the bed", done: false }, { label: "Walk the dog", done: false },{ label: "Do the replits", done: false }]
+	const [todos, setTodos] = useState([])
+	// const tareas = [{ label: "Make the bed", done: false }, { label: "Walk the dog", done: false },{ label: "Do the replits", done: false }]
+	const [tarea, setTarea] = useState('')
 
-	// fetch('https://playground.4geeks.com/apis/fake/todos/user/max', {
-	// 	method: "PUT",
-	// 	body: tareas,
-	// 	headers: {
-	// 		"Content-Type": "application/json"
-	// 	}
-	// })
+	async function addUser() {
+		try {
+			let response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/maxgoxt', {
+				method: 'POST',
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify([]),
+			})
+			let data = await response.json()
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async function addTask() {
+		try {
+			let response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/maxgoxt', {
+				method: 'PUT',
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify(todos),
+			})
+			let data = await response.json()
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	async function getTask() {
+		try {
+			const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/maxgoxt')
+			const data = await response.json()
+			setTodos(data);
+			console.log(data);
+			console.log(todos);
+		} catch (error) {
+			console.log("error");
+		}
+	}
 
 	useEffect(() => {
 		// addUser()
 		getTask()
-	},[])
+	}, [])
 
-	async function getTask() {
-		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/maxgoxt')
-		// , {
-		// 	method: "POST",
-		// 	body: JSON.stringify([]),
-		// 	headers: {
-		// 		"Content-Type": "application/json"
-		// 	}
-		// })
-		const data = await response.json()
-		console.log(data);}
+	useEffect(() => {
+		addTask()
+	}, [todos])
 
-	async function addUser() {
-		const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/margotxt', {
-			method: "POST",
-			body: JSON.stringify([]),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-		const data = await response.json()
-		console.log(data);
+	function add(e) {
+		if (e.key === "Enter" && tarea !== "") {
+			setTodos(todos.concat({ label: tarea, done: false }))
+			setTarea("")
+		}
+	}
+
+	function del(item) {
+		setTodos(todos.filter((x, newIndex) =>
+			item != newIndex
+		))
+	}
+
+	return (
+		<div className="w-50 mx-auto">
+			<h1 className="contatiner fw-light">To Do List</h1>
+			<input className="form-control fst-italic" type="text" placeholder='¿Que tarea pendiente tenés?'
+				value={tarea}
+				onChange={(e) => { setTarea(e.target.value) }}
+				onKeyDown={add} />
+			<ul className="list-group">
+				{
+					todos.map((item, index) => {
+						return (
+							<li className="list-group-item d-flex justify-content-between fst-italic" key={index}>{item.label}
+								<i className="fa fa-trash" style={{ color: '#c70000' }} onClick={() => del(index)}></i>
+							</li>)
+					})}
+			</ul>
+			<small className="fw-light">{todos.length} tareas pendientes</small>
+		</div>
+	);
+};
+
+export default Home;
+
+
+
+
+	// async function getTask() {
+	// 	const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/maxgoxt')
+	// 	// , {
+	// 	// 	method: "POST",
+	// 	// 	body: JSON.stringify([]),
+	// 	// 	headers: {
+	// 	// 		"Content-Type": "application/json"
+	// 	// 	}
+	// 	// })
+	// 	const data = await response.json()
+	// 	console.log(data);}
+
+	// async function addUser() {
+	// 	const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/maxgoxt', {
+	// 		method: "POST",
+	// 		body: JSON.stringify([]),
+	// 		headers: {
+	// 			"Content-Type": "application/json"
+	// 		}
+	// 	})
+	// 	const data = await response.json()
+	// 	console.log(data);
+
+
+
 			// .then(resp => {
 			// 	console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
 			// 	console.log(resp.status); // el código de estado = 200 o código = 400 etc.
@@ -59,13 +139,3 @@ const Home = () => {
 			// 	//manejo de errores
 			// 	console.log(error);
 			// });
-		}
-
-	return (
-		<div className="text-center">
-			<h1>Hello World</h1>
-		</div>
-	);
-};
-
-export default Home;
